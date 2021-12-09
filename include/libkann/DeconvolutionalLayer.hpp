c9b9b3573226118b0f52ef69ecae90a7723d8e5e
KenKwok <kenkinming2002@gmail.com>
2021-12-09 15:44:48 +0800

    Implement DeconvolutionalLayer

---
+++ include/libkann/DeconvolutionalLayer.hpp
@@ -0,0 +1,87 @@
+#pragma once
+
+#include <functional>
+#include <libkann/export.hpp>
+#include <libkann/Layer.hpp>
+#include <libkann/serialization/Eigen.hpp>
+
+#include <Eigen/Eigen>
+
+#include <cereal/types/vector.hpp>
+
+#include <random>
+#include <assert.h>
+
+namespace kann
+{
+  class DeconvolutionalLayer : public Layer
+  {
+  public:
+    LIBKANN_SYMEXPORT DeconvolutionalLayer() = default;
+    LIBKANN_SYMEXPORT DeconvolutionalLayer(size_t inputWidth, size_t inputHeight, size_t kernelSize, size_t inputChannelCount, size_t outputChannelCount);
+
+  public:
+    LIBKANN_SYMEXPORT size_t inputSize() const override;
+    LIBKANN_SYMEXPORT size_t outputSize() const override;
+
+  public:
+    void randomize(std::default_random_engine& prng) override;
+
+  public:
+    LIBKANN_SYMEXPORT Eigen::VectorXd feedForward() override;
+    LIBKANN_SYMEXPORT Eigen::RowVectorXd backPropagate(const Eigen::RowVectorXd& outputGradient) override;
+    LIBKANN_SYMEXPORT void train(double learningRate) override;
+
+  public:
+    LIBKANN_SYMEXPORT std::unique_ptr<Layer> cross(const Layer& other, std::default_random_engine& engine, double mutationRate) const override;
+
+  private:
+    Eigen::MatrixXd& kernel(size_t inputChannelIndex, size_t outputChannelIndex)
+    {
+      return m_kernels[outputChannelIndex * m_inputChannelCount + inputChannelIndex];
+    }
+
+    const Eigen::MatrixXd& kernel(size_t inputChannelIndex, size_t outputChannelIndex) const
+    {
+      return m_kernels[outputChannelIndex * m_inputChannelCount + inputChannelIndex];
+    }
+
+    Eigen::MatrixXd& kernelGradient(size_t inputChannelIndex, size_t outputChannelIndex)
+    {
+      return m_kernelsGradient[outputChannelIndex * m_inputChannelCount + inputChannelIndex];
+    }
+
+    const Eigen::MatrixXd& kernelGradient(size_t inputChannelIndex, size_t outputChannelIndex) const
+    {
+      return m_kernelsGradient[outputChannelIndex * m_inputChannelCount + inputChannelIndex];
+    }
+
+  public:
+    template<typename Archive>
+    void serialize(Archive& archive)
+    {
+      archive(cereal::base_class<Layer>(this));
+
+      archive(m_inputWidth);
+      archive(m_inputHeight);
+      archive(m_inputChannelCount);
+      archive(m_outputChannelCount);
+
+      archive(m_kernels);
+      archive(m_kernelsGradient);
+    }
+
+  private:
+    size_t m_inputWidth,  m_inputHeight;
+    size_t m_kernelSize;
+    size_t m_inputChannelCount;
+    size_t m_outputChannelCount;
+
+  private:
+    std::vector<Eigen::MatrixXd> m_kernels;
+    std::vector<Eigen::MatrixXd> m_kernelsGradient;
+  };
+}
+
+CEREAL_REGISTER_TYPE(kann::DeconvolutionalLayer);
+CEREAL_REGISTER_POLYMORPHIC_RELATION(kann::Layer, kann::DeconvolutionalLayer);
---
+++ src/DeconvolutionalLayer.cpp
@@ -0,0 +1,125 @@
+#include <libkann/DeconvolutionalLayer.hpp>
+
+#include <assert.h>
+
+namespace kann
+{
+  DeconvolutionalLayer::DeconvolutionalLayer(size_t inputWidth, size_t inputHeight, size_t kernelSize, size_t inputChannelCount, size_t outputChannelCount)
+    : m_inputWidth(inputWidth), m_inputHeight(inputHeight), m_kernelSize(kernelSize), m_inputChannelCount(inputChannelCount), m_outputChannelCount(outputChannelCount)
+  {
+    auto kernelsCount = m_inputChannelCount * m_outputChannelCount;
+    m_kernels        .resize(kernelsCount, Eigen::MatrixXd::Zero(m_kernelSize, m_kernelSize));
+    m_kernelsGradient.resize(kernelsCount, Eigen::MatrixXd::Zero(m_kernelSize, m_kernelSize));
+  }
+
+  size_t DeconvolutionalLayer::inputSize() const
+  {
+    return m_inputWidth * m_inputHeight * m_inputChannelCount;
+  }
+
+  size_t DeconvolutionalLayer::outputSize() const
+  {
+    return (m_inputWidth+m_kernelSize-1) * (m_inputHeight+m_kernelSize-1) * m_outputChannelCount;
+  }
+
+  void DeconvolutionalLayer::randomize(std::default_random_engine& engine)
+  {
+    std::uniform_real_distribution<double> dist(-0.1,0.1);
+    for(auto& kernel : m_kernels)
+      kernel = Eigen::MatrixXd::NullaryExpr(kernel.rows(), kernel.cols(), [&](){
+        return dist(engine);
+      });
+  }
+
+  Eigen::VectorXd DeconvolutionalLayer::feedForward()
+  {
+    // Well, we do deconvolutions
+    Eigen::VectorXd output = Eigen::VectorXd::Zero(this->outputSize());
+
+    for(size_t inputChannelIndex = 0; inputChannelIndex<m_inputChannelCount; ++inputChannelIndex)
+      for(size_t outputChannelIndex = 0; outputChannelIndex<m_outputChannelCount; ++outputChannelIndex)
+      {
+        auto outputWidth = m_inputWidth+m_kernelSize-1;
+        auto outputHeight = m_inputHeight+m_kernelSize-1;
+
+        Eigen::Map<const Eigen::MatrixXd> inputChannel (this->input().data()  + inputChannelIndex  * m_inputWidth * m_inputHeight, m_inputHeight, m_inputWidth);
+        Eigen::Map<Eigen::MatrixXd>       outputChannel(output.data()   + outputChannelIndex * outputWidth  * outputHeight,  outputHeight,  outputWidth);
+        for(size_t i=0; i<m_inputWidth; ++i)
+          for(size_t j=0; j<m_inputHeight; ++j)
+          {
+            const auto& kernel = this->kernel(inputChannelIndex, outputChannelIndex);
+            outputChannel.block(j,i, m_kernelSize, m_kernelSize) += inputChannel(j,i) * kernel;
+          }
+      }
+
+    return output;
+  }
+
+  Eigen::RowVectorXd DeconvolutionalLayer::backPropagate(const Eigen::RowVectorXd& outputGradient)
+  {
+    assert(outputGradient.size() == this->outputSize());
+
+    Eigen::RowVectorXd inputGradient = Eigen::VectorXd::Zero(this->inputSize());
+
+    for(size_t inputChannelIndex = 0; inputChannelIndex<m_inputChannelCount; ++inputChannelIndex)
+      for(size_t outputChannelIndex = 0; outputChannelIndex<m_outputChannelCount; ++outputChannelIndex)
+      {
+        auto outputWidth = m_inputWidth+m_kernelSize-1;
+        auto outputHeight = m_inputHeight+m_kernelSize-1;
+
+        Eigen::Map<const Eigen::MatrixXd> inputChannel         (this->input().data()        + inputChannelIndex  * m_inputWidth * m_inputHeight, m_inputHeight, m_inputWidth);
+        Eigen::Map<Eigen::MatrixXd>       inputGradientChannel (inputGradient.data()  + inputChannelIndex  * m_inputWidth * m_inputHeight, m_inputHeight, m_inputWidth);
+
+        Eigen::Map<const Eigen::MatrixXd> outputGradientChannel(outputGradient.data() + outputChannelIndex * outputWidth  * outputHeight,  outputHeight,  outputWidth);
+        for(size_t i=0; i<m_inputWidth; ++i)
+          for(size_t j=0; j<m_inputHeight; ++j)
+          {
+            const auto& kernel   = this->kernel(inputChannelIndex, outputChannelIndex);
+            auto& kernelGradient = this->kernelGradient(inputChannelIndex, outputChannelIndex);
+            inputGradientChannel(j,i) += outputGradientChannel.block(j,i, m_kernelSize, m_kernelSize).cwiseProduct(kernel).sum();
+            kernelGradient += outputGradientChannel.block(j,i, m_kernelSize, m_kernelSize) * inputChannel(j,i);
+          }
+      }
+
+    return inputGradient;
+  }
+
+  void DeconvolutionalLayer::train(double learningRate)
+  {
+    for(size_t i=0; i<m_kernels.size(); ++i)
+    {
+      m_kernels[i] -= learningRate * m_kernelsGradient[i];
+      m_kernelsGradient[i].setZero();
+    }
+  }
+
+  std::unique_ptr<Layer> DeconvolutionalLayer::cross(const Layer& _other, std::default_random_engine& engine, double mutationRate) const
+  {
+    auto result = std::make_unique<DeconvolutionalLayer>(*this);
+    const auto& other = dynamic_cast<const DeconvolutionalLayer&>(_other);
+
+    std::uniform_int_distribution<> distribution(0, 1);
+    std::uniform_real_distribution<double> mutationDistribution(0.0, 1.0);
+    std::uniform_real_distribution<double> weightDistribution(-1.0, 1.0);
+
+    assert(m_kernels.size() != 0);
+    assert(m_kernels.size() == other.m_kernels.size());
+    for(size_t channel = 0; channel<m_kernels.size(); ++channel)
+    {
+      auto& resultKernel = result->m_kernels[channel];
+      const auto& kernel       = m_kernels[channel];
+      const auto& otherKernel  = other.m_kernels[channel];
+
+      assert(kernel.size() != 0);
+      assert(kernel.size() == otherKernel.size());
+      for(long i=0; i<resultKernel.size(); ++i)
+      {
+        resultKernel.data()[i] = distribution(engine) == 0 ? kernel.data()[i] : otherKernel.data()[i];
+        if(mutationDistribution(engine) < mutationRate)
+          resultKernel.data()[i] = weightDistribution(engine);
+      }
+    }
+
+    return result;
+  }
+}
