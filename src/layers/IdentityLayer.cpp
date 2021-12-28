4b0caece4e63109905ec3b7402bc3f5d44001b8c
KenKwok <kenkinming2002@gmail.com>
2021-12-28 15:39:34 +0800

    Support increasing size(with zero padding) in IdentityLayer

--- src/layers/IdentityLayer.cpp
+++ src/layers/IdentityLayer.cpp
@@ -5,10 +5,7 @@ namespace kann
   using namespace std::placeholders;
 
   IdentityLayer::IdentityLayer(size_t inputSize, size_t outputSize, size_t offset)
-    : Layer(0), m_inputSize(inputSize), m_outputSize(outputSize), m_offset(offset)
-  {
-    assert(inputSize >= outputSize+offset);
-  }
+    : Layer(0), m_inputSize(inputSize), m_outputSize(outputSize), m_offset(offset) {}
 
   std::unique_ptr<Layer> IdentityLayer::clone() const
   {
@@ -27,13 +24,22 @@ namespace kann
 
   void IdentityLayer::feedForward(const Eigen::VectorXd& input, Eigen::VectorXd& output) const
   {
-    output = input.segment(m_offset, m_outputSize);
+    output = Eigen::VectorXd::Zero(m_outputSize);
+
+    if(m_inputSize >= m_outputSize)
+      output = input.segment(m_offset, m_outputSize);
+    else
+      output.segment(m_offset, m_inputSize) = input;
   }
 
   void IdentityLayer::backPropagate(const Eigen::VectorXd& input, const Eigen::RowVectorXd& outputGradient, Eigen::RowVectorXd& inputGradient, Eigen::ArrayXd& layerGradient) const
   {
     inputGradient = Eigen::RowVectorXd::Zero(m_inputSize);
-    inputGradient.segment(m_offset, m_outputSize) = outputGradient;
+    if(m_inputSize >= m_outputSize)
+      inputGradient.segment(m_offset, m_outputSize) = outputGradient;
+    else
+      inputGradient = outputGradient.segment(m_offset, m_inputSize);
+
     layerGradient.resize(0); // We do not have any gradient
   }
 }
