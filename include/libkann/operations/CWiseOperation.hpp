805763123c0bbfc56b80a4ea0aba77da5fd63698
KenKwok <kenkinming2002@gmail.com>
2022-02-06 18:40:04 +0800

    Reimplement CWiseOperation to use CRTP black magic

--- include/libkann/operations/CWiseOperation.hpp
+++ include/libkann/operations/CWiseOperation.hpp
@@ -3,37 +3,95 @@
 #include <libkann/operations/CWiseProductOperation.hpp>
 #include <libkann/Operation.hpp>
 
+#include <type_traits>
+#include <utility>
+
 namespace kann
 {
-  struct AssertFunc
-  {
-    double operator()(double val) const { assert(false); }
-  };
-
-  template<typename ForwardFunc, typename BackwardFunc>
-  class CWiseOperation : public UnaryOperation
+  /* CRTP Black magic */
+  template<typename Derived, size_t Count>
+  class CWiseOperation : public Operation
   {
   public:
-    CWiseOperation(ForwardFunc forwardFunc = ForwardFunc(), BackwardFunc backwardFunc = BackwardFunc())
-      : m_forwardFunc(forwardFunc), m_backwardFunc(backwardFunc) {}
-
-  public:
-    Tensor processImpl(const Tensor& input) const override
+    std::shared_ptr<const Tensor> process(std::vector<const Tensor*> inputs) const override
     {
-      Tensor result(input.size());
-      result.asArray() = input.asArray().unaryExpr(m_forwardFunc);
-      return result;
+      return _process(std::move(inputs), std::make_index_sequence<Count>{});
     }
 
-    VariableHandle gradientsImpl(VariableHandle gradient, VariableHandle input) const override
+    VariableList gradients(VariableHandle gradient, VariableList inputs) const override
     {
-      const auto tmp = std::make_shared<const Variable>(std::vector{input}, std::make_shared<CWiseOperation<BackwardFunc, AssertFunc>>(m_backwardFunc, AssertFunc()));
-      const auto result = std::make_shared<const Variable>(std::vector{gradient, tmp}, std::make_shared<CWiseProductOperation>());
+      return _gradients(std::move(gradient), std::move(inputs), std::make_index_sequence<Count>{});
+    }
+
+  private:
+    const Derived& derived() const { return static_cast<const Derived&>(*this); }
+
+  private:
+    template<size_t... Ints>
+    std::shared_ptr<const Tensor> _process(std::vector<const Tensor*> inputs, std::index_sequence<Ints...>) const
+    {
+      const size_t size = inputs.front()->size();
+      auto result = std::make_shared<Tensor>(size);
+      for(size_t i=0; i<size; ++i)
+        result->asArray()(i) = derived().forward(inputs[Ints]->asArray()(i)...);
+
       return result;
     }
 
   private:
-    [[no_unique_address]] ForwardFunc m_forwardFunc;
-    [[no_unique_address]] BackwardFunc m_backwardFunc;
+    // I need to generate required gradient operation
+    class GradientOperation : public Operation
+    {
+    public:
+      constexpr GradientOperation(size_t index, Derived op) : m_index(index), m_op(op) {}
+
+    public:
+      std::shared_ptr<const Tensor> process(std::vector<const Tensor*> inputs) const override
+      {
+        return _process(std::move(inputs), std::make_index_sequence<Count+1>{});
+      }
+
+      VariableList gradients(VariableHandle gradient, VariableList inputs) const override
+      {
+        assert(false && "Unimplemented");
+      }
+
+    private:
+      template<size_t... Ints>
+      std::shared_ptr<const Tensor> _process(std::vector<const Tensor*> inputs, std::index_sequence<Ints...>) const
+      {
+        const size_t size = inputs.front()->size();
+        auto result = std::make_shared<Tensor>(size);
+        for(size_t i=0; i<size; ++i)
+          result->asArray()(i) = m_op.derived().backward(m_index, inputs[Ints]->asArray()(i)...);
+
+        return result;
+      }
+
+    private:
+      size_t m_index;
+      Derived m_op;
+    };
+
+    template<size_t... Ints>
+    VariableList _gradients(VariableHandle gradient, VariableList inputs, std::index_sequence<Ints...> seq) const
+    {
+      VariableList realInput;
+      realInput.push_back(std::move(gradient));
+      realInput.insert(realInput.end(),
+        std::move_iterator(inputs.begin()),
+        std::move_iterator(inputs.end())
+      );
+
+      VariableList result;
+      result.reserve(Count);
+      auto f = [&](size_t i)
+      {
+        auto variable = std::make_shared<const Variable>(realInput, std::make_shared<GradientOperation>(i, derived()));
+        result.push_back(std::move(variable));
+      };
+      (f(Ints), ...);
+      return result;
+    }
   };
 }
