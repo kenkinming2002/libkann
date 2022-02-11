90d447b35330969ae1cf36ce473e93e8384817be
KenKwok <kenkinming2002@gmail.com>
2022-02-11 19:10:45 +0800

    Add NewAgent base class

---
+++ examples/chinesechess/NewAgent.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "Board.hpp"
+
+class NewAgent
+{
+public:
+  virtual ~NewAgent() = default;
+
+public:
+  virtual Board::Move selectMove(const Board& board, Board::Cell::Color color) = 0;
+};
