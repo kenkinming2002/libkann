0364e71dd0ae5a352c22b38c5c15a3b959b3c8f7
KenKwok <kenkinming2002@gmail.com>
2022-02-13 18:57:13 +0800

    [examples/chinesechess] Add alpha-beta pruning to MinimaxAgent

--- examples/chinesechess/MinimaxAgent.cpp
+++ examples/chinesechess/MinimaxAgent.cpp
@@ -5,45 +5,67 @@
 
 #include <iostream>
 
-static std::pair<double, std::optional<Board::Move>> minimax(Board& board, Board::Cell::Color color, size_t depth)
+template<typename Callback>
+auto scopeGuard(Callback callback)
 {
-  auto moves = board.enumerateMove(color);
+  struct ScopeGuard
+  {
+    constexpr ScopeGuard(Callback callback) : callback(callback) {}
+    constexpr ~ScopeGuard() { callback(); }
 
+    constexpr ScopeGuard(const ScopeGuard&) = delete;
+    constexpr ScopeGuard(ScopeGuard&&) = delete;
+
+    constexpr ScopeGuard& operator=(const ScopeGuard&) = delete;
+    constexpr ScopeGuard& operator=(ScopeGuard&&) = delete;
+
+    Callback callback;
+  };
+
+  return ScopeGuard(callback);
+}
+
+static Board::Cell::Color opponentColor(Board::Cell::Color color)
+{
+  return color == Board::Cell::Color::RED ? Board::Cell::Color::BLACK : Board::Cell::Color::RED;
+}
+
+static std::pair<double, std::optional<Board::Move>> maximize(Board& board, Board::Cell::Color color, double maximum, size_t depth)
+{
+  static constexpr double WIN_SCORE = 1e10;
+
+  if(depth == 0)
+  {
+    const auto [scoreRed, scoreBlack] = board.estimateScore();
+    const double score = color == Board::Cell::Color::RED
+      ? scoreRed - scoreBlack
+      : scoreBlack - scoreRed;
+
+    return {score, std::nullopt};
+  }
+
+  const auto moves = board.enumerateMove(color);
+  if(moves.empty())
+    return {-WIN_SCORE, std::nullopt}; // We ran out of move
+
+  // Recursive
   double bestScore = -std::numeric_limits<double>::infinity();
   std::optional<Board::Move> bestMove;
-
-  size_t i = 0;
   for(const auto& move : moves)
   {
-    std::cout << "Depth:" << depth << ", i:" << i++ << "/" << moves.size() << '\n';
-
     const auto cell = board.performMove(move);
+    const auto guard = scopeGuard([&](){ board.undoMove(move, cell); });
 
     if(cell.type == Board::Cell::Type::GENERAL)
     {
-      // We capture the enemy general, and thus we win
-      bestScore = std::numeric_limits<double>::infinity();
+      bestScore = WIN_SCORE;
       bestMove = move;
     }
     else
     {
-      double score;
-      if(depth == 0)
-      {
-        const auto [scoreRed, scoreBlack] = board.estimateScore();
-        score = color == Board::Cell::Color::RED
-          ? scoreRed - scoreBlack
-          : scoreBlack - scoreRed;
-      }
-      else
-      {
-        const auto opponentColor = color == Board::Cell::Color::RED
-          ? Board::Cell::Color::BLACK
-          : Board::Cell::Color::RED;
-        const auto [opponentScore, _] = minimax(board, opponentColor, depth - 1);
-        score = -opponentScore;
-      }
-
+      const auto opponentColor = ::opponentColor(color);
+      const auto [opponentScore, _] = maximize(board, opponentColor, -bestScore, depth - 1);
+      const auto score = -opponentScore;
       if(bestScore < score)
       {
         bestScore = score;
@@ -51,17 +73,18 @@ static std::pair<double, std::optional<Board::Move>> minimax(Board& board, Board
       }
     }
 
-    board.undoMove(move, cell);
+    // Alpha-beta pruning
+    if(bestScore >= maximum)
+      break;
   }
 
-  return {bestScore, *bestMove};
+  return {bestScore, bestMove};
 }
 
 Board::Move MinimaxAgent::selectMove(const Board& board, Board::Cell::Color color)
 {
   Board copy = board;
-
-  auto [bestScore, bestMove] = minimax(copy, color, 3);
+  auto [bestScore, bestMove] = maximize(copy, color, std::numeric_limits<double>::infinity(), 5);
   assert(bestMove);
   return *bestMove;
 }
