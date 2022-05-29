1197473cef4894c56f200203e12c84a8449c06c6
KenKwok <kenkinming2002@gmail.com>
2022-05-30 02:20:02 +0800

    Solve static intialization order fiasco problem in LayerDef save/load

--- src/LayerDef.cpp
+++ src/LayerDef.cpp
@@ -3,28 +3,39 @@
 #include <range/v3/all.hpp>
 
 #include <typeindex>
+#include <iostream>
 
 namespace kann
 {
-  static std::unordered_map<std::type_index, LayerDef::save_t> save_map;
-  static std::unordered_map<std::string,     LayerDef::load_t> load_map;
+  auto& save_map()
+  {
+    static std::unordered_map<std::type_index, LayerDef::save_t> instance;
+    return instance;
+  }
+
+  auto& load_map()
+  {
+    static std::unordered_map<std::string, LayerDef::load_t> instance;
+    return instance;
+  }
 
   void LayerDef::register_save_load(std::string name, const std::type_info& type_info, save_t save, load_t load)
   {
-    save_map.emplace(std::type_index(type_info), save);
-    load_map.emplace(std::move(name), load);
+    std::cout << "Registration" << std::endl;
+    save_map().emplace(std::type_index(type_info), save);
+    load_map().emplace(std::move(name), load);
   }
 
   YAML::Node LayerDef::save(layer_def_t layer)
   {
     auto type_index = std::type_index(typeid(*layer));
-    return save_map.at(type_index)(layer);
+    return save_map().at(type_index)(layer);
   }
 
   layer_def_t LayerDef::load(YAML::Node node)
   {
     auto name = node["type"].as<std::string>();
-    return load_map.at(name)(node);
+    return load_map().at(name)(node);
   }
 
   layer_def_t LayerDef::load(const std::string& filename)
