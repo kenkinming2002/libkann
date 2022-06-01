dce5f1ba713ad4c700d365ed78ab9d95b2998b1c
KenKwok <kenkinming2002@gmail.com>
2022-06-01 16:30:37 +0800

    Fix ScaleOperation gradient to not take reciprocal

--- src/operations/ScaleOperation.cpp
+++ src/operations/ScaleOperation.cpp
@@ -16,6 +16,6 @@ namespace kann
   variable_t ScaleOperation::gradientsImpl(variable_t gradient, variable_t input) const
   {
     assert(m_val != 0.0);
-    return std::make_shared<const Variable>(std::vector{gradient}, std::make_shared<ScaleOperation>(1.0/m_val));
+    return std::make_shared<const Variable>(std::vector{gradient}, std::make_shared<ScaleOperation>(m_val));
   }
 }
