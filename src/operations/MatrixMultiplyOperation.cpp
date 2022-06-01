13a8a70c6d2fa30a682a5d670ff9dc2cf1a85bd9
KenKwok <kenkinming2002@gmail.com>
2022-06-01 15:39:52 +0800

    Remove special case handling for matrix vector product
    This is because that is handled internally since Eigen 3.4

--- src/operations/MatrixMultiplyOperation.cpp
+++ src/operations/MatrixMultiplyOperation.cpp
@@ -10,23 +10,7 @@ namespace kann
   Tensor MatrixMultiplyOperation::processImpl(const Tensor& a, const Tensor& b) const
   {
     Tensor result(m_m * m_n);
-    if(m_n == 1)
-    {
-      // This is actually just a matrix vector multiplication
-      if(m_transpose1)
-        result.asVector().noalias() = a.asMatrix(m_k, m_m).transpose() * b.asVector();
-      else
-        result.asVector().noalias() = a.asMatrix(m_m, m_k) * b.asVector();
-    }
-    else if(m_m == 1)
-    {
-      // This is also just a matrix vector multiplication
-      if(m_transpose2)
-        result.asRowVector().noalias() = a.asRowVector() * b.asMatrix(m_n, m_k).transpose();
-      else
-        result.asRowVector().noalias() = a.asRowVector() * b.asMatrix(m_k, m_n);
-    }
-    else if(m_k == 1)
+    if(m_k == 1)
     {
       // a is a mx1 matrix, b is a 1xn matrix
       result.asMatrix(m_m,m_n).noalias() = a.asVector() * b.asRowVector();
