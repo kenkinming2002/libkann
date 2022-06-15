855d00b9fc4fabd483cb9c54f5cb3f3d95ed7f26
KenKwok <kenkinming2002@gmail.com>
2022-06-15 21:07:44 +0800

    Reimplement SimpleOptimizer with new Tensor API

--- include/libkann/optimizers/SimpleOptimizer.hpp
+++ include/libkann/optimizers/SimpleOptimizer.hpp
@@ -11,7 +11,7 @@ namespace kann
     SimpleOptimizer(double learningRate);
 
   public:
-    size_t process(Graph& graph, Info& info, size_t size, size_t index, size_t gradient_index) const override;
+    size_t process(Graph& graph, Info& info, Shape shape, size_t index, size_t gradient_index) const override;
 
   private:
     double m_learningRate;
