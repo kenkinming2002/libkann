c480c31a7177b4fa13efda6c64a228b2e89969bc
KenKwok <kenkinming2002@gmail.com>
2022-06-20 11:00:54 +0800

    [NoGraph] Add new LayerDef API

--- include/libkann/LayerDef.hpp
+++ include/libkann/LayerDef.hpp
@@ -3,6 +3,7 @@
 #include <libkann/Export.hpp>
 
 #include <libkann/Types.hpp>
+#include <libkann/Tensor.hpp>
 #include <libkann/Tag.hpp>
 #include <libkann/Shape.hpp>
 
@@ -15,6 +16,7 @@
 
 namespace kann
 {
+  struct Layer;
   struct LayerDef : public std::enable_shared_from_this<LayerDef>
   {
   public:
@@ -44,29 +46,14 @@ namespace kann
     KANN_EXPORT virtual ~LayerDef() = default;
 
   public:
+    KANN_EXPORT virtual std::shared_ptr<LayerStorage> create(std::default_random_engine& prng) const = 0;
+
+  public:
     KANN_EXPORT virtual Shape input_shape() const = 0;
     KANN_EXPORT virtual Shape output_shape() const = 0;
 
   public:
-    struct Info
-    {
-      std::vector<Shape>  parameter_shapes;
-      std::vector<Tag>    parameter_tags;
-      std::vector<size_t> parameter_indices;
-
-      std::vector<Shape> state_shapes;
-      std::vector<size_t> input_state_indices;
-      std::vector<size_t> output_state_indices;
-
-      KANN_EXPORT void add_parameter(Shape shape, Tag tag, size_t index);
-      KANN_EXPORT void add_state(Shape shape, size_t input_index, size_t output_index);
-
-      KANN_EXPORT void add_parameters(Shape shape, Tag tag, const std::vector<size_t>& indices);
-      KANN_EXPORT void add_states(Shape shape, const std::vector<size_t>& input_indices, const std::vector<size_t>& output_indices);
-    };
-
-  public:
-    KANN_EXPORT virtual std::shared_ptr<LayerStorage> create(std::default_random_engine& prng) const = 0;
-    KANN_EXPORT virtual size_t batch_process(Graph& graph, Info& info, size_t batch_size, size_t input_index) const { assert(false && "Unimplemented"); }
+    KANN_EXPORT virtual Tensor forward(Layer& layer, Tensor input) const = 0;
+    KANN_EXPORT virtual Tensor backward(Layer& layer, Tensor output_gradient) const = 0;
   };
 }
