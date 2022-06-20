a950592fde18f39f61e2df318fbedc47e300d49e
KenKwok <kenkinming2002@gmail.com>
2022-06-20 17:01:32 +0800

    [NoGraph] Add Layer::sub_layers and Layer::create_from()

--- include/libkann/Layer.hpp
+++ include/libkann/Layer.hpp
@@ -9,8 +9,14 @@ namespace kann
   struct KANN_EXPORT Layer : public Function
   {
   public:
+    KANN_EXPORT static std::shared_ptr<Layer> create_from(std::shared_ptr<const LayerDef> def, std::shared_ptr<LayerStorage> storage);
+
+  public:
     std::shared_ptr<const LayerDef> def;
     std::shared_ptr<LayerStorage> storage;
+
+  public:
+    std::vector<std::shared_ptr<Layer>> sub_layers;
 
   public:
     KANN_EXPORT Tensor forward(Tensor inputs) override;
--- src/Layer.cpp
+++ src/Layer.cpp
@@ -2,6 +2,15 @@
 
 namespace kann
 {
+  std::shared_ptr<Layer> Layer::create_from(std::shared_ptr<const LayerDef> def, std::shared_ptr<LayerStorage> storage)
+  {
+    std::shared_ptr<Layer> layer = std::make_shared<Layer>();
+    layer->def     = std::move(def);
+    layer->storage = std::move(storage);
+    layer->sub_layers = ranges::views::transform(def->sub_layer_defs, storage->sub_layer_storages, &Layer::create_from) | ranges::to_vector;
+    return layer;
+  }
+
   Tensor Layer::forward(Tensor inputs)
   {
     return this->def->forward(*this, std::move(inputs));
