b320f88fc8f9976101e99a51774b3bf6d896ce32
KenKwok <kenkinming2002@gmail.com>
2022-06-20 15:33:03 +0800

    [NoGraph] Add missing KANN_EXPORT to batch() and unbatch()

--- include/libkann/Batch.hpp
+++ include/libkann/Batch.hpp
@@ -1,11 +1,12 @@
 #pragma once
 
+#include <libkann/Export.hpp>
 #include <libkann/Tensor.hpp>
 
 #include <vector>
 
 namespace kann
 {
-  std::vector<Tensor> batch(const std::vector<Tensor>& values, size_t batch_size);
-  std::vector<Tensor> unbatch(const std::vector<Tensor>& values, size_t batch_size);
+  KANN_EXPORT std::vector<Tensor> batch(const std::vector<Tensor>& values, size_t batch_size);
+  KANN_EXPORT std::vector<Tensor> unbatch(const std::vector<Tensor>& values, size_t batch_size);
 }
