6fb7bf5fb352b828724138cb8f1e9e4690cb16d5
KenKwok <kenkinming2002@gmail.com>
2022-06-20 17:07:07 +0800

    [NoGraph] Fix use after move in Layer::create_from()

--- src/Layer.cpp
+++ src/Layer.cpp
@@ -5,9 +5,9 @@ namespace kann
   std::shared_ptr<Layer> Layer::create_from(std::shared_ptr<const LayerDef> def, std::shared_ptr<LayerStorage> storage)
   {
     std::shared_ptr<Layer> layer = std::make_shared<Layer>();
-    layer->def     = std::move(def);
-    layer->storage = std::move(storage);
     layer->sub_layers = ranges::views::transform(def->sub_layer_defs, storage->sub_layer_storages, &Layer::create_from) | ranges::to_vector;
+    layer->def        = std::move(def);
+    layer->storage    = std::move(storage);
     return layer;
   }
 
