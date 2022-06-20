a6d6deacc514983b53afc29e09e8a5ee588808b0
KenKwok <kenkinming2002@gmail.com>
2022-06-20 22:10:37 +0800

    Add trapping on NaN on initialize()

--- src/Initialize.cpp
+++ src/Initialize.cpp
@@ -7,10 +7,14 @@
 #include <libkann/layer_defs/Sequential.hpp>
 #include <libkann/layer_defs/Dense.hpp>
 
+#include <fenv.h>
+
 namespace kann
 {
   void initialize()
   {
+    feenableexcept(FE_DIVBYZERO | FE_INVALID | FE_OVERFLOW);
+
     LayerDef::register_save_load<ActivationLayerDef>("activation");
     LayerDef::register_save_load<ConvolutionalLayerDef>("convolution");
     LayerDef::register_save_load<SequentialLayerDef>("sequential");
