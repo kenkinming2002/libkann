0da19a69f30f7a29afcce99949f9e43db19b7913
KenKwok <kenkinming2002@gmail.com>
2022-06-20 15:53:34 +0800

    [NoGraph] Add step() method to Optimizer

--- include/libkann/Optimizer.hpp
+++ include/libkann/Optimizer.hpp
@@ -10,6 +10,9 @@ namespace kann
   {
   public:
     KANN_EXPORT virtual ~Optimizer() = default;
+
+  public:
+    KANN_EXPORT virtual void step() {}
     KANN_EXPORT virtual void optimize(Variable& variable) const = 0;
   };
 }
