767b1eba37f870b0cd53903b6434d9dc74ef2c9e
KenKwok <kenkinming2002@gmail.com>
2022-06-21 21:12:13 +0800

    [Math] Implement templated math::transform analogous to math::cwise

--- include/libkann/Math.hpp
+++ include/libkann/Math.hpp
@@ -14,7 +14,35 @@ namespace kann::math
 
   KANN_EXPORT void broadcast(TensorRef from, MutableTensorRef to, Operation operation, Direction direction, double value);
   KANN_EXPORT void reduce(TensorRef from, MutableTensorRef to, Operation operation, Direction direction, double value);
+
   KANN_EXPORT void transform(TensorRef from, MutableTensorRef to, Operation operation, double value);
+
+  template<typename Func>
+  void transform(TensorRef a, MutableTensorRef to, Func func)
+  {
+    assert(a.shape() == to.shape());
+    for(size_t i=0; i<to.size(); ++i)
+      to.get(i) = func(a.get(i));
+  }
+
+  template<typename Func>
+  void transform(TensorRef a, TensorRef b, MutableTensorRef to, Func func)
+  {
+    assert(a.shape() == to.shape());
+    assert(b.shape() == to.shape());
+    for(size_t i=0; i<to.size(); ++i)
+      to.get(i) = func(a.get(i), b.get(i));
+  }
+
+  template<typename Func>
+  void transform(TensorRef a, TensorRef b, TensorRef c, MutableTensorRef to, Func func)
+  {
+    assert(a.shape() == to.shape());
+    assert(b.shape() == to.shape());
+    assert(c.shape() == to.shape());
+    for(size_t i=0; i<to.size(); ++i)
+      to.get(i) = func(a.get(i), b.get(i), c.get(i));
+  }
 
   KANN_EXPORT void product(TensorRef a, bool tranpose_a, TensorRef b, bool transpose_b, MutableTensorRef c);
 
