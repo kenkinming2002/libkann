83aaa31aa7906adbb64b1460736b5e0d35fbf6ee
KenKwok <kenkinming2002@gmail.com>
2022-06-23 23:58:15 +0800

    [TensorV2] Use new Tensor API in SimpleOptimizer

--- src/optimizers/SimpleOptimizer.cpp
+++ src/optimizers/SimpleOptimizer.cpp
@@ -10,6 +10,6 @@ namespace kann
   void SimpleOptimizer::optimize(Variable& variable) const
   {
     assert(variable.gradient);
-    math::transform<1>(variable.value.as_ref(), {variable.gradient->as_ref()}, math::FMA(-m_learningRate));
+    math::transform<1>(variable.value.as_ref(), {variable.gradient->as_const_ref()}, math::FMA(-m_learningRate));
   }
 }
