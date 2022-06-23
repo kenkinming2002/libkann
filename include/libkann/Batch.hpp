bef2391354d9978fc0dd765eb82852c4fee6aae8
KenKwok <kenkinming2002@gmail.com>
2022-06-24 02:01:24 +0800

    [TensorV2] Reimplement batch/unbatch with new Tensor API

--- include/libkann/Batch.hpp
+++ include/libkann/Batch.hpp
@@ -7,6 +7,6 @@
 
 namespace kann
 {
-  KANN_EXPORT std::vector<Tensor> batch(const std::vector<Tensor>& values, size_t batch_size);
-  KANN_EXPORT std::vector<Tensor> unbatch(const std::vector<Tensor>& values, size_t batch_size);
+  KANN_EXPORT std::vector<Tensor<float>> batch(const std::vector<Tensor<float>>& values, size_t batch_size);
+  KANN_EXPORT std::vector<Tensor<float>> unbatch(const std::vector<Tensor<float>>& values, size_t batch_size);
 }
--- src/Batch.cpp
+++ src/Batch.cpp
@@ -5,52 +5,37 @@
 
 namespace kann
 {
-  std::vector<Tensor> batch(const std::vector<Tensor>& values, size_t batch_size)
+  std::vector<Tensor<float>> batch(const std::vector<Tensor<float>>& values, size_t batch_size)
   {
-    const Shape shape = Shape::concat(Shape(batch_size), values.front().shape());
-    if(batch_size != 1)
-    {
-      return values
-        | ranges::views::take_exactly(values.size() / batch_size * batch_size) // Ensure that the each chunk is full
-        | ranges::views::chunk(batch_size)
-        | ranges::views::transform([&](auto&& sub_values){
-            MutableTensor result = MutableTensor::create(shape);
-            for(auto&& [i, value] : ranges::views::enumerate(std::forward<decltype(sub_values)>(sub_values)))
-              std::copy_n(value.data(), value.size(), result.as_ref()[i].data());
+    const Shape shape = Shape::concat(Shape(batch_size), values.front().as_ref().shape());
+    return values
+      | ranges::views::take_exactly(values.size() / batch_size * batch_size) // Ensure that the each chunk is full
+      | ranges::views::chunk(batch_size)
+      | ranges::views::transform([&](auto&& sub_values){
+          Tensor<float> result = Tensor<float>::create(shape);
+          for(auto&& [i, value] : ranges::views::enumerate(std::forward<decltype(sub_values)>(sub_values)))
+            std::copy_n(value.as_ref().data(), value.as_ref().size(), result.as_ref()[i].data());
 
-            return result.as_const();
-          })
-        | ranges::to_vector;
-    }
-    else
-    {
-      return values
-        | ranges::views::transform([&](const Tensor& value){ return value.reshape(shape); })
-        | ranges::to_vector;
-    }
+          return result;
+        })
+      | ranges::to_vector;
   }
 
-  std::vector<Tensor> unbatch(const std::vector<Tensor>& values, size_t batch_size)
+  std::vector<Tensor<float>> unbatch(const std::vector<Tensor<float>>& values, size_t batch_size)
   {
-    const Shape shape = values.front().shape().drop_front(1);
-    if(batch_size != 1)
-    {
-      return values
-        | ranges::views::transform([&](const Tensor& value) {
-            return ranges::views::ints(0zu, batch_size)
-              | ranges::views::transform([&](size_t i) { return value[i]; })
-              | ranges::to_vector;
-          })
-        | ranges::views::cache1
-        | ranges::views::join
-        | ranges::to_vector;
-    }
-    else
-    {
-      return values
-        | ranges::views::transform([&](const Tensor& value){ return value.reshape(shape); })
-        | ranges::to_vector;
-    }
+    std::vector<Tensor<float>> results;
+    results.reserve(values.size() * batch_size);
+    for(const Tensor<float>& value : values)
+      for(size_t i=0; i<batch_size; ++i)
+      {
+        TensorRef<const float> value_slice = value.as_const_ref()[i];
+
+        Tensor<float> result = Tensor<float>::create(value_slice.shape());
+        std::copy_n(value_slice.data(), value_slice.size(), result.as_ref().data());
+        results.push_back(std::move(result));
+      }
+
+    return results;
   }
 }
 
