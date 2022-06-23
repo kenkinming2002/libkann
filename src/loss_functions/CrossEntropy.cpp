542e8679f4321ed5d1a418af025405815d723a7e
KenKwok <kenkinming2002@gmail.com>
2022-06-23 23:49:16 +0800

    [TensorV2] Use new Tensor API in CrossEntropyLossFunction

--- src/loss_functions/CrossEntropy.cpp
+++ src/loss_functions/CrossEntropy.cpp
@@ -4,34 +4,35 @@
 
 namespace kann
 {
-  Tensor CrossEntropyLossFunction::forward(Tensor inputs)
+  Tensor<float> CrossEntropyLossFunction::forward(Tensor<float> inputs)
   {
     assert(this->expected_outputs);
-    saved_tensors = { inputs };
 
-    MutableTensor outputs = MutableTensor::create(inputs.shape().front(1));
-    outputs.fill(0.0);
-    math::reduce<2>(outputs.as_ref(), {inputs.as_ref(), this->expected_outputs->as_ref()}, math::Direction::RIGHT, [](float output, float input, float expected_output)
+    Tensor<float> outputs = Tensor<float>::create(inputs.as_ref().shape().front(1));
+    outputs.as_ref().fill(0.0);
+    math::reduce<2>(outputs.as_ref(), {inputs.as_const_ref(), this->expected_outputs->as_const_ref()}, math::Direction::RIGHT, [](float output, float input, float expected_output)
     {
       // Assume inputs is properly normalized via the use of a normalization layer such as softmax
       // Entropy/Information of input * Probability of getting such input
       return output - std::log(input) * expected_output;
     });
-    return outputs.as_const();
+
+    saved_tensors = { std::move(inputs) };
+    return outputs;
   }
 
-  Tensor CrossEntropyLossFunction::backward(Tensor output_gradients)
+  Tensor<float> CrossEntropyLossFunction::backward(Tensor<float> output_gradients)
   {
     assert(this->expected_outputs);
-    const Tensor& inputs = saved_tensors[0];
+    const Tensor<float>& inputs = saved_tensors[0];
 
-    MutableTensor input_gradients = MutableTensor::create(inputs.shape());
-    math::transform<2>(input_gradients.as_ref(), {inputs.as_ref(), this->expected_outputs->as_ref()},[](float /*input_gradient*/, float input, float expected_output)
+    Tensor<float> input_gradients = Tensor<float>::create(inputs.as_ref().shape());
+    math::transform<2>(input_gradients.as_ref(), {inputs.as_const_ref(), this->expected_outputs->as_const_ref()},[](float /*input_gradient*/, float input, float expected_output)
     {
       return - expected_output / input;
     });
-    math::broadcast<1>(input_gradients.as_ref(), {output_gradients.as_ref()}, math::Direction::RIGHT, math::MUL);
-    return input_gradients.as_const();
+    math::broadcast<1>(input_gradients.as_ref(), {output_gradients.as_const_ref()}, math::Direction::RIGHT, math::MUL);
+    return input_gradients;
   }
 }
 
