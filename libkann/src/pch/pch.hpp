fb43345368605acc18a280164f6a5c2c45ebc4e7
KenKwok <kenkinming2002@gmail.com>
2022-06-28 02:46:06 +0800

    Use precompiled header in libkann

---
+++ libkann/src/pch/pch.hpp
@@ -0,0 +1,21 @@
+#include <libtensor/Tensor.hpp>
+#include <libtensor/Shape.hpp>
+#include <libtensor/Math.hpp>
+#include <libtensor/Utils.hpp>
+
+#include <range/v3/all.hpp>
+#include <fmt/core.h>
+#include <yaml-cpp/yaml.h>
+
+#include <vector>
+#include <optional>
+#include <string>
+#include <string_view>
+#include <random>
+#include <memory>
+#include <compare>
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <assert.h>
+
