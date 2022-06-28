6743e8805f33d1489342a33b3c5818054247b560
KenKwok <kenkinming2002@gmail.com>
2022-06-28 17:39:35 +0800

    [libtensor-backend] Implement Eigen backend with only sgemm

---
+++ libtensor/backends/eigen.cpp
@@ -0,0 +1,30 @@
+#include <Eigen/Eigen>
+
+extern "C"
+{
+  using Matrix = Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>;
+
+  void sgemm(size_t M, size_t N, size_t K,
+      const float* A, bool trans_a,
+      const float* B, bool trans_b,
+      float* C)
+  {
+    auto a = trans_a ? Matrix::Map(A, K, M) : Matrix::Map(A, M, K);
+    auto b = trans_b ? Matrix::Map(A, N, K) : Matrix::Map(A, K, N);
+    auto c = Matrix::Map(C, M, N);
+    if(trans_a)
+    {
+      if(trans_b)
+        c = a.transpose() * b.transpose();
+      else
+        c = a.transpose() * b;
+    }
+    else
+    {
+      if(trans_b)
+        c = a * b.transpose();
+      else
+        c = a * b;
+    }
+  }
+}
