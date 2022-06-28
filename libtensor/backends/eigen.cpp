353c1878338e91b53d676df2813e0f85ccb48394
KenKwok <kenkinming2002@gmail.com>
2022-06-28 18:41:19 +0800

    [libtensor-backend] Implement sgeconv2d and sgecorr2d with Eigen

--- libtensor/backends/eigen.cpp
+++ libtensor/backends/eigen.cpp
@@ -1,4 +1,5 @@
 #include <Eigen/Eigen>
+#include <unsupported/Eigen/CXX11/Tensor>
 
 extern "C"
 {
@@ -26,5 +27,65 @@ extern "C"
       else
         c = a * b;
     }
+  }
+
+  void sgecorr2d(size_t M, size_t N, size_t K,
+      const float* inputs,  size_t height_input, size_t width_input, bool trans_inputs,
+      const float* kernels, size_t height_kernel, size_t width_kernel, bool trans_kernels,
+      float* outputs, size_t height_output, size_t width_output)
+  {
+    for(size_t m=0; m<M; ++m)
+      for(size_t n=0; n<N; ++n)
+        for(size_t k=0; k<K; ++k)
+        {
+          const size_t input_index  = trans_inputs  ? (k * M + m) : (m * K + k);
+          const size_t kernel_index = trans_kernels ? (n * K + k) : (k * N + n);
+          const size_t output_index = m * N + n;
+
+          Eigen::TensorMap<const Eigen::Tensor<float, 2>> input (&inputs [input_index  * height_input  * width_input],  height_input,  width_input);
+          Eigen::TensorMap<const Eigen::Tensor<float, 2>> kernel(&kernels[kernel_index * height_kernel * width_kernel], height_kernel, width_kernel);
+          Eigen::TensorMap<      Eigen::Tensor<float, 2>> output(&outputs[output_index * height_output * width_output], height_output, width_output);
+
+          const size_t height_padding = (height_output - height_input + height_kernel - 1) / 2;
+          const size_t width_padding  = (width_output  - width_input  + width_kernel  - 1) / 2;
+
+          Eigen::array<std::pair<int, int>, 2> paddings;
+          paddings[0] = std::make_pair(height_padding, height_padding);
+          paddings[1] = std::make_pair(width_padding,  width_padding);
+          auto padded_input = input.pad(paddings);
+
+          const Eigen::array<ptrdiff_t, 2> dims({0, 1});
+          output = padded_input.convolve(kernel.reverse(dims), dims);
+        }
+  }
+
+  void sgeconv2d(size_t M, size_t N, size_t K,
+      const float* inputs,  size_t height_input, size_t width_input, bool trans_inputs,
+      const float* kernels, size_t height_kernel, size_t width_kernel, bool trans_kernels,
+      float* outputs, size_t height_output, size_t width_output)
+  {
+    for(size_t m=0; m<M; ++m)
+      for(size_t n=0; n<N; ++n)
+        for(size_t k=0; k<K; ++k)
+        {
+          const size_t input_index  = trans_inputs  ? (k * M + m) : (m * K + k);
+          const size_t kernel_index = trans_kernels ? (n * K + k) : (k * N + n);
+          const size_t output_index = m * N + n;
+
+          Eigen::TensorMap<const Eigen::Tensor<float, 2>> input (&inputs [input_index  * height_input  * width_input],  height_input,  width_input);
+          Eigen::TensorMap<const Eigen::Tensor<float, 2>> kernel(&kernels[kernel_index * height_kernel * width_kernel], height_kernel, width_kernel);
+          Eigen::TensorMap<      Eigen::Tensor<float, 2>> output(&outputs[output_index * height_output * width_output], height_output, width_output);
+
+          const size_t height_padding = (height_output - height_input + height_kernel - 1) / 2;
+          const size_t width_padding  = (width_output  - width_input  + width_kernel  - 1) / 2;
+
+          Eigen::array<std::pair<int, int>, 2> paddings;
+          paddings[0] = std::make_pair(height_padding, height_padding);
+          paddings[1] = std::make_pair(width_padding,  width_padding);
+          auto padded_input = input.pad(paddings);
+
+          const Eigen::array<ptrdiff_t, 2> dims({0, 1});
+          output = padded_input.convolve(kernel, dims);
+        }
   }
 }
