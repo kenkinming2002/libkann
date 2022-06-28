6adcad9c0d21a97997e74a44c2f270952e884ef3
KenKwok <kenkinming2002@gmail.com>
2022-06-28 19:59:48 +0800

    [libtensor-backend] Use sgecorr2d and sgeconv2d from backend

--- libtensor/src/Math.cpp
+++ libtensor/src/Math.cpp
@@ -35,6 +35,50 @@ namespace tensor::math
     return impl(M, N, K, A, trans_a, B, trans_b, C);
   }
 
+  static inline sgecorr2d_t resolve_sgecorr2d()
+  {
+    for(const Backend& backend : backends())
+      if(backend.sgecorr2d)
+        return backend.sgecorr2d;
+
+    spdlog::error("libtensor : No backend provide sgecorr2d function");
+    exit(EXIT_FAILURE);
+  }
+
+  static inline void sgecorr2d(size_t M, size_t N, size_t K,
+      const float* inputs,  size_t height_input, size_t width_input, bool trans_inputs,
+      const float* kernels, size_t height_kernel, size_t width_kernel, bool trans_kernels,
+      float* outputs, size_t height_output, size_t width_output)
+  {
+    static sgecorr2d_t impl = resolve_sgecorr2d();
+    return impl(M, N, K,
+        inputs,  height_input,  width_input,  trans_inputs,
+        kernels, height_kernel, width_kernel, trans_kernels,
+        outputs, height_output, width_output);
+  }
+
+  static inline sgeconv2d_t resolve_sgeconv2d()
+  {
+    for(const Backend& backend : backends())
+      if(backend.sgeconv2d)
+        return backend.sgeconv2d;
+
+    spdlog::error("libtensor : No backend provide sgeconv2d function");
+    exit(EXIT_FAILURE);
+  }
+
+  static inline void sgeconv2d(size_t M, size_t N, size_t K,
+      const float* inputs,  size_t height_input, size_t width_input, bool trans_inputs,
+      const float* kernels, size_t height_kernel, size_t width_kernel, bool trans_kernels,
+      float* outputs, size_t height_output, size_t width_output)
+  {
+    static sgeconv2d_t impl = resolve_sgeconv2d();
+    return impl(M, N, K,
+        inputs,  height_input,  width_input,  trans_inputs,
+        kernels, height_kernel, width_kernel, trans_kernels,
+        outputs, height_output, width_output);
+  }
+
   void product(Tensor<float> dst,
       Tensor<const float> a, bool transpose_a,
       Tensor<const float> b, bool transpose_b)
@@ -60,56 +104,14 @@ namespace tensor::math
     sgemm(M, N, K, a.data(), transpose_a, b.data(), transpose_b, dst.data());
   }
 
-  inline Tensor<const float> pad(Tensor<const float> inputs, size_t padding_height, size_t padding_width)
-  {
-    if(padding_height == 0 && padding_width == 0)
-      return inputs; // Fast path
-
-    const auto [M, N] = std::make_pair(inputs.dimension(0), inputs.dimension(1));
-    const auto [input_height, input_width]   = std::make_pair(inputs.dimension(2), inputs.dimension(3));
-    const auto [output_height, output_width] = std::make_pair(input_height + 2 * padding_height,
-                                                              input_width +  2 * padding_width);
-
-    Tensor<float> outputs = Tensor<float>::create(Shape{M, N, output_height, output_width});
-    for(size_t j=0; j<M; ++j)
-      for(size_t i=0; i<N; ++i)
-      {
-        // Zero-fill
-        for(size_t y=0; y<padding_height; ++y)
-          for(size_t x=0; x<output_width; ++x)
-            outputs(j, i, y, x) = 0.0f;
-
-        for(size_t y=padding_height; y<padding_height+input_height; ++y)
-        {
-          // Zero fill
-          for(size_t x=0; x<padding_width; ++x)
-            outputs(j, i, y, x) = 0.0f;
-
-          for(size_t x=padding_width; x<padding_width+input_width; ++x)
-            outputs(j, i, y, x) = inputs(j, i, y - padding_height, x - padding_width);
-
-          // Zero fill
-          for(size_t x=padding_width + input_width; x<output_width; ++x)
-            outputs(j, i, y, x) = 0.0f;
-        }
-
-        // Zero-fill
-        for(size_t y=padding_height+input_height; y<output_height; ++y)
-          for(size_t x=0; x<output_width; ++x)
-            outputs(j, i, y, x) = 0.0f;
-      }
-
-    return outputs;
-  }
-
   void image2d_operation(Tensor<float> outputs,
       Tensor<const float> inputs, bool transpose_inputs,
       Tensor<const float> kernels, bool transpose_kernels,
       Image2DOperation operation)
   {
-    auto [M1, N1, output_height, output_width] = std::make_tuple(outputs.dimension(0), outputs.dimension(1), outputs.dimension(2), outputs.dimension(3));
-    auto [M2, K1, input_height,  input_width ] = std::make_tuple(inputs .dimension(0), inputs .dimension(1), inputs .dimension(2), inputs .dimension(3));
-    auto [K2, N2, kernel_height, kernel_width] = std::make_tuple(kernels.dimension(0), kernels.dimension(1), kernels.dimension(2), kernels.dimension(3));
+    size_t M1 = outputs.dimension(0), N1 = outputs.dimension(1), output_height = outputs.dimension(2), output_width = outputs.dimension(3);
+    size_t M2 = inputs .dimension(0), K1 = inputs .dimension(1), input_height  = inputs .dimension(2), input_width  = inputs .dimension(3);
+    size_t K2 = kernels.dimension(0), N2 = kernels.dimension(1), kernel_height = kernels.dimension(2), kernel_width = kernels.dimension(3);
 
     if(transpose_inputs)  std::swap(M2, K1);
     if(transpose_kernels) std::swap(K2, N2);
@@ -119,41 +121,20 @@ namespace tensor::math
     assert(K1 == K2);
 
     const auto [M, N, K] = std::make_tuple(M1, N1, K1);
-    //const auto [output_size, input_size, kernel_size] = std::make_tuple(Vec2(P1, P2), Vec2(Q1, Q2), Vec2(R1, R2));
-
-    const auto [padding_height, padding_width] = std::make_pair((output_height - input_height + kernel_height - 1) / 2,
-                                                                (output_width  - input_width  + kernel_width  - 1) / 2);
-
-    inputs = pad(std::move(inputs), padding_height, padding_width);
-
-    // It would a mystery if the following code works the first time.
-    // Nevertheless, we plan to replace it with algorithm using FFT anyway hopefully soonish
-    outputs.fill(0.0);
-    for(size_t j=0; j<M; ++j)
-      for(size_t i=0; i<N; ++i)
-        for(size_t k=0; k<K; ++k)
-          for(size_t oy=0; oy<output_height; ++oy)
-            for(size_t ox=0; ox<output_width; ++ox)
-            {
-              // We are inside five nested for loop and we can finally start doing the cross_correlation/convolution
-              float sum = 0.0f;
-              for(size_t ky = 0; ky<kernel_height; ++ky)
-                for(size_t kx = 0; kx<kernel_width; ++kx)
-                {
-                  const auto [iy, ix] = std::make_pair(oy + ky, ox + kx); // This disregard padding
-
-                  float input  = transpose_inputs  ? inputs(k, j, iy /*- padding_height*/, ix /*- padding_width*/)
-                                                   : inputs(j, k, iy /*- padding_height*/, ix /*- padding_width*/);
-                                                                                                   //
-                  float kernel = (operation == Image2DOperation::CROSS_CORRELATION) ? (transpose_kernels ? kernels(i, k, ky,                     kx                   ) : kernels(k, i, ky,                     kx                   ))
-                               : (operation == Image2DOperation::CONVOLUTION)       ? (transpose_kernels ? kernels(i, k, kernel_height - ky - 1, kernel_width - kx - 1) : kernels(k, i, kernel_height - ky - 1, kernel_width - kx - 1))
-                               : (assert(false && "Unimplemented"), 0.0f);
-
-                  sum += input * kernel;
-                }
-
-              outputs(j, i, oy, ox) += sum;
-            }
-
+    switch(operation)
+    {
+    case Image2DOperation::CROSS_CORRELATION:
+      sgecorr2d(M, N, K,
+          inputs.data(),  input_height,  input_width,  transpose_inputs,
+          kernels.data(), kernel_height, kernel_width, transpose_kernels,
+          outputs.data(), output_height, output_width);
+      break;
+    case Image2DOperation::CONVOLUTION:
+      sgeconv2d(M, N, K,
+          inputs.data(),  input_height,  input_width,  transpose_inputs,
+          kernels.data(), kernel_height, kernel_width, transpose_kernels,
+          outputs.data(), output_height, output_width);
+      break;
+    }
   }
 }
