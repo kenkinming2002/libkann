0c71755423cc6940790ca1509c2a266c99e3e519
KenKwok <kenkinming2002@gmail.com>
2022-07-15 01:30:01 +0800

    Remove blanket implementation in LossFunction base class

--- libkann/include/libkann/LossFunction.hpp
+++ libkann/include/libkann/LossFunction.hpp
@@ -12,14 +12,5 @@ namespace kann
   public:
     tensor::Shape shape;
     std::optional<tensor::Tensor<const float>> expected_outputs;
-
-  public:
-    KANN_EXPORT tensor::Tensor<const float> forward(tensor::Tensor<const float> inputs) override;
-    KANN_EXPORT tensor::Tensor<const float> backward(tensor::Tensor<const float> output_gradients) override;
-
-  public:
-    // Note: inputs are 2D tensor while output_gradients is 1D
-    KANN_EXPORT virtual tensor::Tensor<const float> loss_forward(tensor::Tensor<const float> inputs, tensor::Tensor<const float> expected_outputs) { assert(false && "Unimplemented"); };
-    KANN_EXPORT virtual tensor::Tensor<const float> loss_backward(tensor::Tensor<const float> inputs, tensor::Tensor<const float> expected_outputs, tensor::Tensor<const float> output_gradients) { assert(false && "Unimplemented"); };
   };
 }
