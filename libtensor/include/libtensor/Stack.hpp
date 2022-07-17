02d8ad5838fe9ad09136e3a20c61ba993a306bb9
KenKwok <kenkinming2002@gmail.com>
2022-07-17 14:20:16 +0800

    Implement stacking of Tensor

--- libtensor/include/libtensor/Stack.hpp
+++ libtensor/include/libtensor/Stack.hpp
@@ -5,12 +5,73 @@
 
 namespace tensor
 {
-  // The raw API - do not manage any dynamic memory
-  template<typename T> LIBTENSOR_EXPORT void stack_outer_raw(size_t M, size_t N, T* output, const T* inputs[]);
-  template<typename T> LIBTENSOR_EXPORT void stack_inner_raw(size_t M, size_t N, T* output, const T* inputs[]);
+  // The raw C-like API - no template magic
+  LIBTENSOR_EXPORT void stack_outer_raw(size_t M, size_t N, char* __restrict__ output, const char* __restrict__ * __restrict__ inputs, size_t size) noexcept;
+  LIBTENSOR_EXPORT void stack_inner_raw(size_t M, size_t N, char* __restrict__ output, const char* __restrict__ * __restrict__ inputs, size_t size) noexcept;
 
-  // The API wraped in a Tensor type
-  template<typename T> LIBTENSOR_EXPORT Tensor<const T> stack_outer(std::vector<Tensor<const T>> values);
-  template<typename T> LIBTENSOR_EXPORT Tensor<const T> stack_inner(std::vector<Tensor<const T>> values);
+  // The raw C++ API - do not manage any dynamic memory
+  //
+  // IDK, maybe I am invoking UB here, but this should work on any reasonable implementation
+  template<typename T>
+  inline void stack_outer(size_t M, size_t N, T* output, const T** inputs) noexcept
+  {
+    stack_outer_raw(M, N, reinterpret_cast<char*>(output), reinterpret_cast<const char**>(inputs), sizeof(T));
+  }
+
+  template<typename T>
+  inline void stack_inner(size_t M, size_t N, T* output, const T** inputs) noexcept
+  {
+    stack_inner_raw(M, N, reinterpret_cast<char*>(output), reinterpret_cast<const char**>(inputs), sizeof(T));
+  }
+
+  // The API wraped in a Tensor type, hopefully, you are using these
+  template<typename T>
+  Tensor<const T> stack_outer(std::vector<Tensor<const T>> values)
+  {
+    if(values.empty() || std::any_of(values.begin(), values.end(), [&](const auto& value) { return values.front().shape() != value.shape(); }))
+      throw std::runtime_error("All shapes must be the same for stacking");
+
+    const auto& shape = values.front().shape();
+    const size_t M = values.size();
+    const size_t N = shape.size();
+    auto result = Tensor<T>::create(Shape::make(M, shape));
+
+    T* output = result.data();
+    const T** inputs = new const T*[values.size()];
+
+    for(size_t i=0; i<values.size(); ++i)
+      inputs[i] = values[i].data();
+
+    stack_outer(M, N, output, inputs);
+
+    delete[] inputs;
+
+    return result;
+  }
+
+  template<typename T>
+  Tensor<const T> stack_inner(std::vector<Tensor<const T>> values)
+  {
+    if(values.empty() || std::any_of(values.begin(), values.end(), [&](const auto& value) { return values.front().shape() != value.shape(); }))
+      throw std::runtime_error("All shapes must be the same for stacking");
+
+    const auto& shape = values.front().shape();
+    const size_t M = values.size();
+    const size_t N = shape.size();
+
+    auto result = Tensor<T>::create(Shape::make(M, shape));
+
+    T* output = result.data();
+    const T** inputs = new const T*[values.size()];
+
+    for(size_t i=0; i<values.size(); ++i)
+      inputs[i] = values[i].data();
+
+    stack_inner(M, N, output, inputs);
+
+    delete[] inputs;
+
+    return result;
+  }
 }
 
--- libtensor/src/Stack.cpp
+++ libtensor/src/Stack.cpp
@@ -1,80 +1,19 @@
 #include <libtensor/Stack.hpp>
 
-// What is a math function
-// There is a problem, and that is that raw pointer is much easier to work with
-// as it does not carry any uncessary information
-
 namespace tensor
 {
-  // Stact input of size N to output of size M*N
-  template<typename T>
-  void stack_outer_raw(size_t M, size_t N, T* output, const T* inputs[])
+  void stack_outer_raw(size_t M, size_t N, char* __restrict__ output, const char* __restrict__ * __restrict__ inputs, size_t size) noexcept
+  {
+    for(size_t m=0; m<M; ++m)
+      std::copy_n(&inputs[m][0], N*size, &output[m*N*size]);
+  }
+
+  void stack_inner_raw(size_t M, size_t N, char* __restrict__ output, const char* __restrict__ * __restrict__ inputs, size_t size) noexcept
   {
     for(size_t m=0; m<M; ++m)
       for(size_t n=0; n<N; ++n)
-        output[m*N+n] = inputs[m][n];
+        std::copy_n(&inputs[m][n*size], size, &output[(n*M+m)*size]);
   }
-
-  template<typename T>
-  void stack_inner_raw(size_t M, size_t N, T* output, const T* inputs[])
-  {
-    for(size_t m=0; m<M; ++m)
-      for(size_t n=0; n<N; ++n)
-        output[n*M+m] = inputs[m][n];
-  }
-
-  template<typename T>
-  Tensor<const T> stack_outer(std::vector<Tensor<const T>> values)
-  {
-    if(values.empty() || std::any_of(values.begin(), values.end(), [&](const auto& value) { return values.front().shape() != value.shape(); }))
-      throw std::runtime_error("All shapes must be the same for stacking");
-
-    const auto& shape = values.front().shape();
-    const size_t M = values.size();
-    const size_t N = shape.size();
-
-    auto result = Tensor<T>::create(Shape::make(M, shape));
-
-    T* output = result.data();
-    std::vector<const T*> inputs = values
-      | ranges::views::transform([](const auto& value) { return value.data(); })
-      | ranges::to_vector;
-
-    stack_outer_raw(M, N, output, inputs.data());
-
-    return result;
-  }
-
-  template<typename T>
-  Tensor<const T> stack_inner(std::vector<Tensor<const T>> values)
-  {
-    if(values.empty() || std::any_of(values.begin(), values.end(), [&](const auto& value) { return values.front().shape() != value.shape(); }))
-      throw std::runtime_error("All shapes must be the same for stacking");
-
-    const auto& shape = values.front().shape();
-    const size_t M = values.size();
-    const size_t N = shape.size();
-
-    auto result = Tensor<T>::create(Shape::make(M, shape));
-
-    T* output = result.data();
-    std::vector<const T*> inputs = values
-      | ranges::views::transform([](const auto& value) { return value.data(); })
-      | ranges::to_vector;
-
-    stack_inner_raw(M, N, output, inputs.data());
-
-    return result;
-  }
-
-  template Tensor<const float> stack_outer(std::vector<Tensor<const float>> values);
-  template Tensor<const float> stack_inner(std::vector<Tensor<const float>> values);
-
-  template Tensor<const double> stack_outer(std::vector<Tensor<const double>> values);
-  template Tensor<const double> stack_inner(std::vector<Tensor<const double>> values);
-
-  template Tensor<const long double> stack_outer(std::vector<Tensor<const long double>> values);
-  template Tensor<const long double> stack_inner(std::vector<Tensor<const long double>> values);
 }
 
 
