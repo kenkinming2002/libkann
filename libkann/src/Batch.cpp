41d8e0834dcad9f14bbff5a6f27dd0afb745890f
KenKwok <kenkinming2002@gmail.com>
2022-07-17 23:09:45 +0800

    Use split_outer for unbatch

--- libkann/src/Batch.cpp
+++ libkann/src/Batch.cpp
@@ -1,6 +1,7 @@
 #include <libkann/Batch.hpp>
 
 #include <libtensor/memops/Stack.hpp>
+#include <libtensor/memops/Split.hpp>
 
 #include <range/v3/all.hpp>
 #include <range/v3/view/join.hpp>
@@ -19,25 +20,11 @@ namespace kann
   std::vector<tensor::Tensor<float>> unbatch(std::vector<tensor::Tensor<float>> values, size_t batch_size)
   {
     std::vector<tensor::Tensor<float>> results;
-
-    const tensor::Shape value_shape  = values.front().shape;
-    const tensor::Shape result_shape = value_shape.drop_front(1);
-    const size_t size = result_shape.size();
-    assert(value_shape.dimension(0) == batch_size);
-    for(size_t i=0; i<values.size(); ++i)
+    for(auto&& value : values)
     {
-      auto value = values[i];
-      auto value_buffer  = value.buffer;
-      for(size_t k=0; k<batch_size; ++k)
-      {
-        auto result_buffer = std::make_shared<tensor::Buffer<float>>(result_shape.size());
-        std::copy_n(
-          value_buffer->data().data() + k * size,
-          size,
-          result_buffer->data().data()
-        );
-        results.push_back(tensor::Tensor<float>(result_shape, std::move(result_buffer)));
-      }
+      auto sub_results = tensor::split_outer(std::move(value));
+      assert(sub_results.size() == batch_size);
+      results.insert(results.end(), sub_results.begin(), sub_results.end());
     }
     return results;
   }
