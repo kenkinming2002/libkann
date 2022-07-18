a237461672d78d376842643d45b1d2d42f35316c
KenKwok <kenkinming2002@gmail.com>
2022-07-18 18:46:26 +0800

    Reimplement memops stack

--- libtensor/include/libtensor/memops/Stack.hpp
+++ libtensor/include/libtensor/memops/Stack.hpp
@@ -5,76 +5,7 @@
 
 namespace tensor
 {
-  // The raw C-like API - no template magic
-  LIBTENSOR_EXPORT void stack_outer_raw(size_t M, size_t N, char* __restrict__ output, const char* __restrict__ * __restrict__ inputs, size_t size) noexcept;
-  LIBTENSOR_EXPORT void stack_inner_raw(size_t M, size_t N, char* __restrict__ output, const char* __restrict__ * __restrict__ inputs, size_t size) noexcept;
-
-  // The raw C++ API - do not manage any dynamic memory
-  //
-  // IDK, maybe I am invoking UB here, but this should work on any reasonable implementation
-  template<typename T>
-  inline void stack_outer(size_t M, size_t N, T* output, const T** inputs) noexcept
-  {
-    stack_outer_raw(M, N, reinterpret_cast<char*>(output), reinterpret_cast<const char**>(inputs), sizeof(T));
-  }
-
-  template<typename T>
-  inline void stack_inner(size_t M, size_t N, T* output, const T** inputs) noexcept
-  {
-    stack_inner_raw(M, N, reinterpret_cast<char*>(output), reinterpret_cast<const char**>(inputs), sizeof(T));
-  }
-
-  // The API wraped in a Tensor type, hopefully, you are using these
-  template<typename T>
-  Tensor<T> stack_outer(std::vector<Tensor<T>> values)
-  {
-    if(values.empty() || std::any_of(values.begin(), values.end(), [&](const auto& value) { return values.front().shape != value.shape; }))
-      throw std::runtime_error("All shapes must be the same for stacking");
-
-    const auto& shape = values.front().shape;
-    const size_t M = values.size();
-    const size_t N = shape.size();
-
-    auto result_buffer = std::make_shared<Buffer<T>>(M * N);
-
-    T* output;
-    const T** inputs = new const T*[values.size()];
-
-    output = result_buffer->data().data();
-    for(size_t i=0; i<values.size(); ++i)
-      inputs[i] = values[i].buffer->data().data();
-
-    stack_outer(M, N, output, inputs);
-
-    delete[] inputs;
-
-    return Tensor<T>(Shape::make(M, shape), std::move(result_buffer));
-  }
-
-  template<typename T>
-  Tensor<T> stack_inner(std::vector<Tensor<T>> values)
-  {
-    if(values.empty() || std::any_of(values.begin(), values.end(), [&](const auto& value) { return values.front().shape != value.shape; }))
-      throw std::runtime_error("All shapes must be the same for stacking");
-
-    const auto& shape = values.front().shape;
-    const size_t M = values.size();
-    const size_t N = shape.size();
-
-    auto result_buffer = std::make_shared<Buffer<T>>(M * N);
-
-    T* output;
-    const T** inputs = new const T*[values.size()];
-
-    output = result_buffer->data().data();
-    for(size_t i=0; i<values.size(); ++i)
-      inputs[i] = values[i].buffer->data().data();
-
-    stack_inner(M, N, output, inputs);
-
-    delete[] inputs;
-
-    return Tensor<T>(Shape::make(M, shape), std::move(result_buffer));
-  }
+  template<typename T> LIBTENSOR_EXPORT Tensor<T> stack_outer(std::vector<Tensor<T>> values);
+  template<typename T> LIBTENSOR_EXPORT Tensor<T> stack_inner(std::vector<Tensor<T>> values);
 }
 
--- libtensor/src/memops/Stack.cpp
+++ libtensor/src/memops/Stack.cpp
@@ -2,18 +2,64 @@
 
 namespace tensor
 {
-  void stack_outer_raw(size_t M, size_t N, char* __restrict__ output, const char* __restrict__ * __restrict__ inputs, size_t size) noexcept
+  template<typename T>
+  static inline void stack_verify_same_shape(const std::vector<Tensor<T>>& values)
   {
-    for(size_t m=0; m<M; ++m)
-      std::copy_n(&inputs[m][0], N*size, &output[m*N*size]);
+    if(values.empty())
+      throw std::runtime_error("Cannot stack empty vector of tensors");
+
+    for(const auto& value : values)
+      if(values.front().shape != value.shape)
+        throw std::runtime_error("All shapes must be the same for stacking");
   }
 
-  void stack_inner_raw(size_t M, size_t N, char* __restrict__ output, const char* __restrict__ * __restrict__ inputs, size_t size) noexcept
+  template<typename T>
+  Tensor<T> stack_outer(std::vector<Tensor<T>> values)
   {
+    stack_verify_same_shape(values);
+
+    const auto& shape = values.front().shape;
+    const size_t M = values.size();
+    const size_t N = shape.size();
+
+    auto result_buffer = std::make_shared<Buffer<T>>(M * N);
     for(size_t m=0; m<M; ++m)
       for(size_t n=0; n<N; ++n)
-        std::copy_n(&inputs[m][n*size], size, &output[(n*M+m)*size]);
+      {
+        const auto& value_buffer = values[m].buffer;
+        result_buffer->data()[m*N+n] = value_buffer->data()[n];
+      }
+
+    return Tensor<T>(Shape::make(M, shape), result_buffer);
   }
+
+  template<typename T>
+  Tensor<T> stack_inner(std::vector<Tensor<T>> values)
+  {
+    stack_verify_same_shape(values);
+
+    const auto& shape = values.front().shape;
+    const size_t M = shape.size();
+    const size_t N = values.size();
+
+    auto result_buffer = std::make_shared<Buffer<T>>(M * N);
+    for(size_t m=0; m<M; ++m)
+      for(size_t n=0; n<N; ++n)
+      {
+        const auto& value_buffer = values[n].buffer;
+        result_buffer->data()[m*N+n] = value_buffer->data()[m];
+      }
+
+    return Tensor<T>(Shape::make(M, shape), result_buffer);
+  }
+
+  template Tensor<float> stack_outer(std::vector<Tensor<float>> values);
+  template Tensor<double> stack_outer(std::vector<Tensor<double>> values);
+  template Tensor<long double> stack_outer(std::vector<Tensor<long double>> values);
+
+  template Tensor<float> stack_inner(std::vector<Tensor<float>> values);
+  template Tensor<double> stack_inner(std::vector<Tensor<double>> values);
+  template Tensor<long double> stack_inner(std::vector<Tensor<long double>> values);
 }
 
 
