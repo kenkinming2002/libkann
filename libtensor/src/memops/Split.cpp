b14cb4c8392f49f70f909537e72b61a8575ac2ef
KenKwok <kenkinming2002@gmail.com>
2022-07-18 19:14:37 +0800

    Cleanup memops split

--- libtensor/src/memops/Split.cpp
+++ libtensor/src/memops/Split.cpp
@@ -6,8 +6,10 @@ namespace tensor
   std::vector<Tensor<T>> split_outer(Tensor<T> value)
   {
     const auto& shape = value.shape;
+    const auto& sub_shape = value.shape.drop_front(1);
+
     const size_t M = shape.dimension(0);
-    const size_t N = shape.drop_front(1).size();
+    const size_t N = sub_shape.size();
 
     // Boilerplate code for type conversion for std vector
     auto results_buffer = std::vector<std::shared_ptr<Buffer<T>>>();
@@ -24,7 +26,7 @@ namespace tensor
 
     auto results = std::vector<Tensor<T>>();
     for(size_t i=0; i<M; ++i)
-      results.push_back(Tensor<T>(shape.drop_front(1), std::move(results_buffer[i])));
+      results.push_back(Tensor<T>(sub_shape, std::move(results_buffer[i])));
     return results;
   }
 
@@ -32,7 +34,9 @@ namespace tensor
   std::vector<Tensor<T>> split_inner(Tensor<T> value)
   {
     const auto& shape = value.shape;
-    const size_t M = shape.drop_back(1).size();
+    const auto& sub_shape = value.shape.drop_back(1);
+
+    const size_t M = sub_shape.size();
     const size_t N = shape.dimension(shape.rank() - 1);
 
     // Boilerplate code for type conversion for std vector
@@ -50,7 +54,7 @@ namespace tensor
 
     auto results = std::vector<Tensor<T>>();
     for(size_t i=0; i<N; ++i)
-      results.push_back(Tensor<T>(shape.drop_front(1), std::move(results_buffer[i])));
+      results.push_back(Tensor<T>(sub_shape, std::move(results_buffer[i])));
     return results;
   }
 
