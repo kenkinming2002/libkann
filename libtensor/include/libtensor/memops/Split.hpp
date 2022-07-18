5ab180d43d031ff193afc8b5a1db8cbabd67a5ab
KenKwok <kenkinming2002@gmail.com>
2022-07-18 19:05:38 +0800

    Reimeplement memops split

--- libtensor/include/libtensor/memops/Split.hpp
+++ libtensor/include/libtensor/memops/Split.hpp
@@ -5,87 +5,8 @@
 
 namespace tensor
 {
-  // The raw C-like API - no template magic
-  LIBTENSOR_EXPORT void split_outer_raw(size_t M, size_t N, char* __restrict__ * __restrict__ outputs, const char* __restrict__ input, size_t size) noexcept;
-  LIBTENSOR_EXPORT void split_inner_raw(size_t M, size_t N, char* __restrict__ * __restrict__ outputs, const char* __restrict__ input, size_t size) noexcept;
-
-  // The raw C++ API - do not manage any dynamic memory
-  //
-  // IDK, maybe I am invoking UB here, but this should work on any reasonable implementation
-  template<typename T>
-  inline void split_outer(size_t M, size_t N, T** output, const T* inputs) noexcept
-  {
-    split_outer_raw(M, N, reinterpret_cast<char**>(output), reinterpret_cast<const char*>(inputs), sizeof(T));
-  }
-
-  template<typename T>
-  inline void split_inner(size_t M, size_t N, T* output, const T** inputs) noexcept
-  {
-    split_inner_raw(M, N, reinterpret_cast<char**>(output), reinterpret_cast<const char*>(inputs), sizeof(T));
-  }
-
-  // The API wraped in a Tensor type, hopefully, you are using these
-  template<typename T>
-  std::vector<Tensor<T>> split_outer(Tensor<T> value)
-  {
-    const auto& shape     = value.shape;
-    const auto& sub_shape = shape.drop_front(1);
-    const size_t M = shape.dimension(0);
-    const size_t N = sub_shape.size();
-
-    auto buffer_results = ranges::views::generate_n([&]() { return std::make_shared<Buffer<T>>(N); }, M) | ranges::to_vector;
-    auto buffer_value   = value.buffer;
-
-    // Raw API
-    {
-      T** outputs = new T*[buffer_results.size()];;
-      const T* input;
-
-      for(size_t i=0; i<buffer_results.size(); ++i)
-        outputs[i] = buffer_results[i]->data().data();
-      input = buffer_value->data().data();
-
-      split_outer(M, N, outputs, input);
-
-      delete[] outputs;
-    }
-
-    return buffer_results
-      | ranges::views::transform([&](auto&& buffer) { return Tensor<T>(Shape::make(sub_shape), std::move(buffer)); })
-      | ranges::to_vector;
-  }
-
-  template std::vector<Tensor<float>> split_outer(Tensor<float> value);
-
-  template<typename T>
-  std::vector<Tensor<T>> split_inner(Tensor<T> value)
-  {
-    const auto& shape     = value.shape;
-    const auto& sub_shape = shape.drop_back(1);
-    const size_t M = shape.dimension(shape.rank() - 1);
-    const size_t N = sub_shape.size();
-
-    auto buffer_results = ranges::views::generate_n([&]() { return std::make_shared<Buffer<T>>(N); }, M) | ranges::to_vector;
-    auto buffer_value   = value.buffer;
-
-    // Raw API
-    {
-      T** outputs = new T*[buffer_results.size()];;
-      const T* input;
-
-      for(size_t i=0; i<buffer_results.size(); ++i)
-        outputs[i] = buffer_results[i]->data().data();
-      input = buffer_value->data().data();
-
-      split_outer(M, N, outputs, input);
-
-      delete[] outputs;
-    }
-
-    return buffer_results
-      | ranges::views::transform([&](auto&& buffer) { return Tensor<T>(Shape::make(sub_shape), std::move(buffer)); })
-      | ranges::to_vector;
-  }
+  template<typename T> LIBTENSOR_EXPORT std::vector<Tensor<T>> split_outer(Tensor<T> value);
+  template<typename T> LIBTENSOR_EXPORT std::vector<Tensor<T>> split_inner(Tensor<T> value);
 }
 
 
--- libtensor/src/memops/Split.cpp
+++ libtensor/src/memops/Split.cpp
@@ -2,16 +2,63 @@
 
 namespace tensor
 {
-  void split_outer_raw(size_t M, size_t N, char* __restrict__ * __restrict__ outputs, const char* __restrict__ input, size_t size) noexcept
+  template<typename T>
+  std::vector<Tensor<T>> split_outer(Tensor<T> value)
   {
-    for(size_t m=0; m<M; ++m)
-      std::copy_n(&input[m*N*size], N*size, &outputs[m][0]);
-  }
+    const auto& shape = value.shape;
+    const size_t M = shape.dimension(0);
+    const size_t N = shape.drop_front(1).size();
 
-  void split_inner_raw(size_t M, size_t N, char* __restrict__ * __restrict__ outputs, const char* __restrict__ input, size_t size) noexcept
-  {
+    // Boilerplate code for type conversion for std vector
+    auto results_buffer = std::vector<std::shared_ptr<Buffer<T>>>();
+    for(size_t i=0; i<M; ++i)
+      results_buffer.push_back(std::make_shared<Buffer<T>>(N));
+
+    auto value_buffer = value.buffer;
     for(size_t m=0; m<M; ++m)
       for(size_t n=0; n<N; ++n)
-        std::copy_n(&input[(n*M+m)*size], size, &outputs[m][n*size]);
+      {
+        const auto& result_buffer = results_buffer[m];
+        (*result_buffer)[n] = (*value_buffer)[m*N+n];
+      }
+
+    auto results = std::vector<Tensor<T>>();
+    for(size_t i=0; i<M; ++i)
+      results.push_back(Tensor<T>(shape.drop_front(1), std::move(results_buffer[i])));
+    return results;
   }
+
+  template<typename T>
+  std::vector<Tensor<T>> split_inner(Tensor<T> value)
+  {
+    const auto& shape = value.shape;
+    const size_t M = shape.drop_back(1).size();
+    const size_t N = shape.dimension(shape.rank() - 1);
+
+    // Boilerplate code for type conversion for std vector
+    auto results_buffer = std::vector<std::shared_ptr<Buffer<T>>>();
+    for(size_t i=0; i<N; ++i)
+      results_buffer.push_back(std::make_shared<Buffer<T>>(M));
+
+    auto value_buffer = value.buffer;
+    for(size_t m=0; m<M; ++m)
+      for(size_t n=0; n<N; ++n)
+      {
+        const auto& result_buffer = results_buffer[n];
+        (*result_buffer)[m] = (*value_buffer)[m*N+n];
+      }
+
+    auto results = std::vector<Tensor<T>>();
+    for(size_t i=0; i<N; ++i)
+      results.push_back(Tensor<T>(shape.drop_front(1), std::move(results_buffer[i])));
+    return results;
+  }
+
+  template std::vector<Tensor<float>> split_outer(Tensor<float> value);
+  template std::vector<Tensor<double>> split_outer(Tensor<double> value);
+  template std::vector<Tensor<long double>> split_outer(Tensor<long double> value);
+
+  template std::vector<Tensor<float>> split_inner(Tensor<float> value);
+  template std::vector<Tensor<double>> split_inner(Tensor<double> value);
+  template std::vector<Tensor<long double>> split_inner(Tensor<long double> value);
 }
