1c48fc36c428c409a6e3643f34f424e4d49dc3d1
KenKwok <kenkinming2002@gmail.com>
2022-07-24 22:14:32 +0800

    Make Layer virtual funtion pure virtual

--- libkann/include/libkann/Layer.hpp
+++ libkann/include/libkann/Layer.hpp
@@ -11,16 +11,16 @@ namespace kann
   // Could I auto-magically generate them? Unfortunately, c++ have no reflection support.
   // All of them should be pretty easy to generate
   public:
-    KANN_EXPORT virtual const LayerDef& get_def() const { assert(false && "Unimplemented"); }
+    KANN_EXPORT virtual const LayerDef& get_def() const = 0;
 
     // Forwarded from LayerDef
-    KANN_EXPORT virtual tensor::Shape get_input_shape()  const { assert(false && "Unimplemented"); }
-    KANN_EXPORT virtual tensor::Shape get_output_shape() const { assert(false && "Unimplemented"); }
+    KANN_EXPORT virtual tensor::Shape get_input_shape()  const = 0;
+    KANN_EXPORT virtual tensor::Shape get_output_shape() const = 0;
 
   public:
-    KANN_EXPORT virtual void initialize(std::default_random_engine& prng) { assert(false && "Unimplemented"); }
+    KANN_EXPORT virtual void initialize(std::default_random_engine& prng) = 0;
 
-    KANN_EXPORT virtual std::unordered_map<std::string, const Variable*> parameters_map() const { assert(false && "Unimplemented"); };
-    KANN_EXPORT virtual std::unordered_map<std::string, Variable*>       parameters_map()       { assert(false && "Unimplemented"); };
+    KANN_EXPORT virtual std::unordered_map<std::string, const Variable*> parameters_map() const = 0;
+    KANN_EXPORT virtual std::unordered_map<std::string, Variable*>       parameters_map()       = 0;
   };
 }
